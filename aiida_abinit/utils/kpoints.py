672243576e56a4bf2eb368d28f55a830b4eebd49
Austin Zadoks <austin.zadoks@pm.me>
2022-04-28 13:52:43 +0000

    First pass towards a bands workchain implementation

--- aiida_abinit/utils/kpoints.py
+++ aiida_abinit/utils/kpoints.py
@@ -5,7 +5,7 @@ import numpy as np
 from aiida import orm
 from aiida.engine import calcfunction
 
-__all__ = ('create_kpoints_from_distance',)
+__all__ = ('create_kpoints_from_distance', 'seekpath_structure_analysis')
 
 
 @calcfunction
@@ -36,3 +36,27 @@ def create_kpoints_from_distance(structure: orm.StructureData, distance: orm.Flo
         kpoints.set_kpoints_mesh([nkpoints, nkpoints, nkpoints])
 
     return kpoints
+
+
+@calcfunction
+def seekpath_structure_analysis(structure, **kwargs):
+    """Primitivize the structure with SeeKpath and generate the high symmetry k-point path through its Brillouin zone.
+
+    This calcfunction will take a structure and pass it through SeeKpath to get the normalized primitive cell and the
+    path of high symmetry k-points through its Brillouin zone. Note that the returned primitive cell may differ from the
+    original structure in which case the k-points are only congruent with the primitive cell.
+    The keyword arguments can be used to specify various Seekpath parameters, such as:
+        with_time_reversal: True
+        reference_distance: 0.025
+        recipe: 'hpkot'
+        threshold: 1e-07
+        symprec: 1e-05
+        angle_tolerance: -1.0
+    Note that exact parameters that are available and their defaults will depend on your Seekpath version.
+    """
+    from aiida.tools import get_explicit_kpoints_path
+
+    # All keyword arugments should be `Data` node instances of base type and so should have the `.value` attribute
+    unwrapped_kwargs = {key: node.value for key, node in kwargs.items() if isinstance(node, orm.Data)}
+
+    return get_explicit_kpoints_path(structure, **unwrapped_kwargs)
